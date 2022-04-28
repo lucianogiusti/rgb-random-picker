2e6f5ca8e9c26906a17c4a763031c168b3484741
lucianogiusti <lucianogiusti@outlook.com.ar>
2022-04-28 18:24:47 -0300

    adding history list

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const colorCuadro = document.querySelector('.cuadro');
+"use strict";
 
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+const colorCuadro = document.querySelector(".cuadro");
 
-const randomColor = () => `rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`;
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
 
-document.querySelector('.btn').addEventListener('click', function(e) {
-    colorCuadro.style.backgroundColor = randomColor();
-    console.log(colorCuadro.style.backgroundColor);
-    document.querySelector('.color-btn').textContent = `Tu color es ${colorCuadro.style.backgroundColor}`;
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+
+document.querySelector(".btn").addEventListener("click", function (e) {
+  colorCuadro.style.backgroundColor = randomColor();
+  console.log(colorCuadro.style.backgroundColor);
+  document.querySelector(
+    ".color-btn"
+  ).textContent = `Tu color es ${colorCuadro.style.backgroundColor}`;
+  const results = [];
+  results.push(colorCuadro.style.backgroundColor);
+  console.log(results);
+  const ul = document.getElementById("lista-historial");
+  const li = document.createElement("li");
+  li.appendChild(document.createTextNode(results));
+  ul.appendChild(li);
 });
+
+/* Removing list items from history */
