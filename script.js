0dc9c8baa9428f70765e59c95a1e331c0ea3c72f
lucianogiusti <lucianogiusti@outlook.com.ar>
2022-04-29 18:47:37 -0300

    fav picks added

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const randomInt = (min, max) =>
 const randomColor = () =>
   `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
 
+const listHistory = document.getElementById("lista-historial");
+const favSelection = document.getElementById("favoritos");
+
 document.querySelector(".btn").addEventListener("click", function (e) {
   colorCuadro.style.backgroundColor = randomColor();
   console.log(colorCuadro.style.backgroundColor);
@@ -22,8 +25,38 @@ document.querySelector(".btn").addEventListener("click", function (e) {
   const birds = document.getElementById("lista-historial");
   const i = birds.childNodes.length;
   console.log(i);
-  const elements = birds.getElementsByTagName('li');
+  const elements = birds.getElementsByTagName("li");
   if (i > 6) birds.removeChild(elements[0]);
 });
 
+document.getElementById("add-fav").addEventListener("click", function (e) {
+  console.log(document.getElementById("lista-historial").lastChild.innerHTML);
+  const ul = document.getElementById("favoritos");
+  const li = document.createElement("li");
+  li.appendChild(
+    document.createTextNode(
+      document.getElementById("lista-historial").lastChild.innerHTML
+    )
+  );
+  ul.appendChild(li);
+  const favColor = document.getElementById("favoritos");
+  const favColorLength = favColor.childNodes.length;
+  const favItems = favColor.getElementsByTagName("li");
+});
 
+/*   if (
+    document.getElementById("lista-historial").lastChild.innerHTML ==
+    document.getElementById("lista-historial").lastChild[-1].innerHTML
+  ) {
+    console.log("si es");
+  }
+  for (
+    let i = 0;
+    i < document.getElementById("lista-historial").children.length;
+    i++
+  ) {
+    if (
+      document.getElementById("lista-historial").lastChild.textContent == 
+    );
+  }
+}); */
